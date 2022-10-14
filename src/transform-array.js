255023b3da2215d33b6c6acf721b22653a0ab63b
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2022-10-14 13:23:00 +0300

    solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error( "'arr' parameter must be an instance of the Array!");
+  }
+  let result = [];
+
+  for (i=0; i<arr.length; i++){
+/*    if (typeof arr[i] == 'number' && isNaN(arr[i])==false) { */
+if (arr[i]!='--discard-next' && arr[i]!='--discard-prev' && arr[i]!='--double-next' && arr[i]!='--double-prev' && arr[i]!=undefined){
+      result.push(arr[i]);
+     }; 
+    if (arr[i]=='--discard-next') {
+      i++;
+    };
+    if (arr[i]=='--discard-prev') {
+      if(arr[i-2]!='--discard-next') {
+      result = result.slice(0,-1);
+      }
+    };
+    if (arr[i]=='--double-next') {
+      if (typeof arr[i+1] == 'number' && isNaN(arr[i+1])==false) {
+      result.push(arr[i+1]);
+      }
+    };
+    if (arr[i]=='--double-prev') {
+      if (typeof arr[i-1] == 'number' && isNaN(arr[i-1])==false && arr[i-2]!='--discard-next') {
+      result.push( arr[i-1]);
+      }
+    };
+
+  }
+  return result;
 }
+
 
 module.exports = {
   transform
