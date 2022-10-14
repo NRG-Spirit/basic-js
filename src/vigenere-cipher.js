60aa34b42d9b0397b250c12627cf484fa03f09b9
NRG-Spirit <102809707+NRG-Spirit@users.noreply.github.com>
2022-10-14 19:00:41 +0300

    solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(rev) {
+    this.reverse = false;
+    if (rev == false) {this.reverse = true};
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if(!str || !key) {
+      throw new Error('Incorrect arguments!')
+    };
+    
+    let encrypted = '';
+    let count = 0;
+
+    while (key.length < str.length) {
+      key += key;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+      let currentLetter = str[i];
+        currentLetter = currentLetter.toUpperCase();
+
+      if (currentLetter.charCodeAt() > 64 && currentLetter.charCodeAt() < 91) {
+          let p = (currentLetter.charCodeAt());
+          let k = (key[count].toUpperCase().charCodeAt());
+          let upperLetter = ((p + k) % 26);
+          encrypted += String.fromCharCode(upperLetter + 65);
+          count++;
+      } else  {
+          encrypted += currentLetter;
+      }
+    }
+
+    if (this.reverse == false) {
+      return encrypted;
+    } else {
+      return encrypted.split('').reverse().join('');
+    }
+  }
+  
+  decrypt(str, key) {
+    if(str === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let decrypted = '';
+    let count = 0;
+
+    while (key.length < str.length) {
+      key += key;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+      let currentLetter = str[i];
+        currentLetter = currentLetter.toUpperCase();
+
+      if (currentLetter.charCodeAt() > 64 && currentLetter.charCodeAt() < 91) {
+        let c = (currentLetter.charCodeAt());
+        let k = (key[count].toUpperCase()).charCodeAt();
+        let upperLetter = ((c - k) + 26) % 26;
+        decrypted += String.fromCharCode(upperLetter + 65);
+        count++;
+      } else {
+          decrypted += currentLetter;
+        }
+    }
+    
+    if (this.reverse == false) {
+      return decrypted;
+    } else {
+      return decrypted.split('').reverse().join('');
+    }
   }
 }
 
